b575a10143c627cde2368b76609ad4f16c6aadc6
David Lam <dav_lam@ymail.com>
2017-01-26 03:11:10 -0500

    finalize scale score reaggreation

--- hiveScript/scaleScore.hql
+++ hiveScript/scaleScore.hql
@@ -3,6 +3,7 @@ USE yelp;
 DROP FUNCTION IF EXISTS scaleReview;
 CREATE FUNCTION scaleReview AS 'edu.rosehulman.rad.ScaleReview' USING JAR 'hdfs:///tmp/jars/RADHiveUDF-0.0.1-SNAPSHOT.jar';
 
-SELECT business_id, scaleReview(r.rating, r.review_time)
-FROM Business AS b, Review AS r
-WHERE b.business_id == r.business_id;
+SELECT b.business_id, scaleReview(r.rating, cast(r.review_time AS string)) AS scaleScore
+FROM BusinessStatic AS b, ReviewStatic AS r
+WHERE b.business_id == r.business_id
+  AND r.review_time IS NOT NULL;
--- hiveUDF/src/main/java/edu/rosehulman/rad/ScaleReview.java
+++ hiveUDF/src/main/java/edu/rosehulman/rad/ScaleReview.java
@@ -7,19 +7,25 @@ import java.util.Date;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.Text;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 
 public class ScaleReview extends UDF {
-	public DoubleWritable evaluate(double score, Date date) throws ParseException {
-//		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-//		DateTime reviewDate = new DateTime(df.parse(date.toString()));
+	public DoubleWritable evaluate(double score, String date) throws ParseException {
+		final double SCALE_START = 100.0;
 		
-		DateTime reviewDate = new DateTime(date);
+		if (date == null) {
+			return null;
+		}
+		
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		
+		DateTime reviewDate = new DateTime(format.parse(date));
 		DateTime currentDate = new DateTime(new Date());
-		double scaleFactor = (double) Days.daysBetween(reviewDate, currentDate).getDays() / 100.0;
-
-		return new DoubleWritable(scaleFactor * score);
+		
+		double daysPassed = Days.daysBetween(reviewDate, currentDate).getDays();
+		double scaleFactor = (daysPassed > SCALE_START) ? (double) daysPassed / SCALE_START : 1;
+		
+		return (scaleFactor == 0) ? new DoubleWritable(score) : new DoubleWritable(score / scaleFactor);
 	}
 }
