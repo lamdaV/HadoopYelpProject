c822d7289df22e1cb4e6b41824987a7c990bf0a1
David Lam <dav_lam@ymail.com>
2017-01-26 21:27:13 -0500

    updated scale hive script

--- hiveScript/scaleScore.hql
+++ hiveScript/scaleScore.hql
@@ -3,7 +3,11 @@ USE yelp;
 DROP FUNCTION IF EXISTS scaleReview;
 CREATE FUNCTION scaleReview AS 'edu.rosehulman.rad.ScaleReview' USING JAR 'hdfs:///tmp/jars/RADHiveUDF-0.0.1-SNAPSHOT.jar';
 
-SELECT b.business_id, scaleReview(r.rating, cast(r.review_time AS string)) AS scaleScore
-FROM BusinessStatic AS b, ReviewStatic AS r
-WHERE b.business_id == r.business_id
-  AND r.review_time IS NOT NULL;
+SELECT scale_table.business_id, AVG(scale_table.scale_score) AS scale_rating
+FROM (SELECT b.business_id AS business_id, scaleReview(r.rating, cast(r.review_time AS string)) AS scale_score
+  FROM BusinessStatic AS b, ReviewStatic AS r
+  WHERE b.business_id == r.business_id
+    AND r.review_time IS NOT NULL
+  ) AS scale_table
+WHERE scale_table.scale_score != -1
+GROUP BY scale_table.business_id;
--- hiveUDF/src/main/java/edu/rosehulman/rad/ScaleReview.java
+++ hiveUDF/src/main/java/edu/rosehulman/rad/ScaleReview.java
@@ -11,11 +11,11 @@ import org.joda.time.DateTime;
 import org.joda.time.Days;
 
 public class ScaleReview extends UDF {
-	public DoubleWritable evaluate(double score, String date) throws ParseException {
+	public double evaluate(double score, String date) throws ParseException {
 		final double SCALE_START = 100.0;
 		
 		if (date == null) {
-			return null;
+			return -1;
 		}
 		
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -24,8 +24,8 @@ public class ScaleReview extends UDF {
 		DateTime currentDate = new DateTime(new Date());
 		
 		double daysPassed = Days.daysBetween(reviewDate, currentDate).getDays();
-		double scaleFactor = (daysPassed > SCALE_START) ? (double) daysPassed / SCALE_START : 1;
+		double scaleFactor = (daysPassed > SCALE_START) ? (double) daysPassed / (daysPassed - SCALE_START) : 1;
 		
-		return (scaleFactor == 0) ? new DoubleWritable(score) : new DoubleWritable(score / scaleFactor);
+		return (scaleFactor == 0) ? (score) : (score / scaleFactor);
 	}
 }
