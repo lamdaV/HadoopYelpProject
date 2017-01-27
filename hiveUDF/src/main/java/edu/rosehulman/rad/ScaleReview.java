2ea7a0b65ec5ff18beef5d35e92c63d99357b418
David Lam <dav_lam@ymail.com>
2017-01-26 22:52:40 -0500

    updated udf weighing to be based on years instead of days, updated scaleScore.hql

--- hiveUDF/src/main/java/edu/rosehulman/rad/ScaleReview.java
+++ hiveUDF/src/main/java/edu/rosehulman/rad/ScaleReview.java
@@ -6,13 +6,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
-import org.apache.hadoop.io.DoubleWritable;
 import org.joda.time.DateTime;
-import org.joda.time.Days;
+import org.joda.time.Years;
 
 public class ScaleReview extends UDF {
 	public double evaluate(double score, String date) throws ParseException {
-		final double SCALE_START = 100.0;
+		final double SCALE_START = 1.0;
 		
 		if (date == null) {
 			return -1;
@@ -23,9 +22,9 @@ public class ScaleReview extends UDF {
 		DateTime reviewDate = new DateTime(format.parse(date));
 		DateTime currentDate = new DateTime(new Date());
 		
-		double daysPassed = Days.daysBetween(reviewDate, currentDate).getDays();
-		double scaleFactor = (daysPassed > SCALE_START) ? (double) daysPassed / (daysPassed - SCALE_START) : 1;
+		double yearsPassed = Years.yearsBetween(reviewDate, currentDate).getYears();
+		double scaleFactor = (yearsPassed > SCALE_START) ? (double) (yearsPassed - SCALE_START) : 1.0;
 		
-		return (scaleFactor == 0) ? (score) : (score / scaleFactor);
+		return score / scaleFactor;
 	}
 }
